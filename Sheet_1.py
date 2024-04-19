08bbc7b720c0a8ede953a791fba3300b481b6a46
Ante Maric <antemaric99@hotmail.com>
2024-04-19 22:21:02 +0200

    Create Sheet_1.py

---
+++ Sheet_1.py
@@ -0,0 +1,12 @@
+# Exercise 1.1: Variables and Basic Data Types
+
+name = "Ante"
+age = 27
+color = "black"
+
+print("My name is {}, I am {} years old, and my favorite color is {}.".format(name, age, color))
+
+
+
+# Exercise 1.2: Control Flow - If-Else Statements
+num = int(input("Please enter a number: "))
